f06ab571ddad1b9107568d3e2074b3123fa14807
Maheswar Maity <maheswarmaity80@gmail.com>
2026-08-15 20:29:58 +0530

    Fix production API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://jobmate-backend-g3i7.onrender.com"
+  baseURL: "https://jobmate-backend-g3i7.onrender.com/api",
 });
 
 export default API;
